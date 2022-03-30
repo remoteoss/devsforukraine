8ff7cd4787a65426dd8a5f6ab667605a427c31d0
Sara Vieira <sara.vieira@remote.com>
2022-03-31 01:21:45 +0200

    add small bios

--- utils/speakers.js
+++ utils/speakers.js
@@ -3,33 +3,44 @@ export const frontendSpeakers = [
     name: "Ben Hong",
     pic: "ben.jpeg",
     twitter: "bencodezen",
+    bio: "@vuejs core team & senior staff dx engineer @netlify",
   },
   {
     name: "Charlie Gerard",
     pic: "charlie.jpeg",
     twitter: "devdevcharlie",
+    bio: "Senior developer advocate  @Stripe",
   },
   {
     name: "Natalia Tepluhina",
     pic: "natalia.jpeg",
     twitter: "N_Tepluhina",
+    bio: "@vuejs core team & Staff Frontend Engineer @gitlab",
   },
   {
     name: "Jessica Janiuk",
     pic: "jessica.jpeg",
     twitter: "ThePunderWoman",
+    bio: "Angular Team @Google",
   },
   {
     name: "Cassidy Williams",
     pic: "cassidy.jpeg",
     twitter: "cassidoo",
+    bio: "Head of DX and Education at @remote",
   },
   {
     name: "Dan Abramov",
     pic: "dan.jpeg",
     twitter: "dan_abramov",
+    bio: "React team at @facebook",
   },
-  { name: "Tomasz Łakomy", pic: "tomasz.jpeg", twitter: "tlakomy" },
+  {
+    name: "Tomasz Łakomy",
+    pic: "tomasz.jpeg",
+    twitter: "tlakomy",
+    bio: "Frontend Engineer at @Stedi",
+  },
 ]
 
 export const backendSpeakers = [
@@ -37,40 +48,48 @@ export const backendSpeakers = [
     name: "Lilly Chen",
     pic: "lilly.jpeg",
     twitter: "lillydoingecon",
+    bio: "Engineer at @contendaCo",
   },
   {
     name: "Sandi Metz",
     pic: "sandi.jpeg",
     twitter: "sandimetz",
+    bio: "Cyclist, Rubyist, author",
   },
   {
     name: "Aaron Patterson",
     pic: "aaron.jpeg",
     twitter: "tenderlove",
+    bio: "Human at @Shopify",
   },
   {
     name: "Saša Jurić",
     pic: "sasa.jpeg",
     twitter: "sasajuric",
+    bio: "Elixir mentor, author of Elixir in Action",
   },
   {
     name: "José Valim",
     pic: "jose.jpeg",
     twitter: "josevalim",
+    bio: "Chief Adoption Officer at @dashbit. Creator of @elixirlang.",
   },
   {
     name: "Andreas Klinger",
     pic: "andreas.jpeg",
     twitter: "andreasklinger",
+    bio: "CTO @beondeck",
   },
   {
     name: "Tetiana",
     pic: "tetiana.jpeg",
     twitter: "Tetiana12345678",
+    bio: "Programmer, in love with Elixir-lang, proud mum, creator of @elixircards and @gitcards",
   },
   {
     name: "Vittoria Bitton",
     pic: "vittoria.jpeg",
     twitter: "vittoria_bitton",
+    bio: "Senior Backend Engineer at @remote",
   },
 ]
