48616d1b28d21370519e94de990399a7f207e7f8
Sara Vieira <sara.vieira@remote.com>
2022-04-01 19:49:40 +0200

    fix types

--- components/Payment/Final.tsx
+++ components/Payment/Final.tsx
@@ -4,62 +4,67 @@ import { useEffect, useRef } from "react"
 import { H1, MutedP } from "../Typography"
 import { Logo } from "../Logo"
 
-function randomInRange(min, max) {
+function randomInRange(min: number, max: number) {
   return Math.random() * (max - min) + min
 }
 var count = 200
 var defaults = {
   origin: { y: 0.7 },
 }
-function fire(particleRatio, opts) {
+function fire(particleRatio: number, opts: any) {
   confetti(
     Object.assign({}, defaults, opts, {
       particleCount: Math.floor(count * particleRatio),
     })
   )
 }
+const realistic = () => {
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  })
+  fire(0.2, {
+    spread: 60,
+  })
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  })
+}
+
+function printMousePos(event: any) {
+  console.log({
+    y: event.clientY / window.innerWidth,
+    x: event.clientX / window.innerHeight,
+  })
+  confetti({
+    particleCount: Math.floor(count * 0.25),
+    angle: randomInRange(55, 125),
+    spread: randomInRange(50, 70),
+    origin: {
+      y: event.clientY / window.innerHeight,
+      x: event.clientX / window.innerWidth,
+    },
+  })
+}
+
 export const Final = () => {
   const router = useRouter()
 
   useEffect(() => {
-    fire(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    })
-    fire(0.2, {
-      spread: 60,
-    })
-    fire(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    })
-    fire(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    })
-    fire(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    })
-
-    function printMousePos(event: any) {
-      console.log({
-        y: event.clientY / window.innerWidth,
-        x: event.clientX / window.innerHeight,
-      })
-      confetti({
-        particleCount: Math.floor(count * 0.25),
-        angle: randomInRange(55, 125),
-        spread: randomInRange(50, 70),
-        origin: {
-          y: event.clientY / window.innerHeight,
-          x: event.clientX / window.innerWidth,
-        },
-      })
-    }
+    realistic()
+    realistic()
 
     document.addEventListener("click", printMousePos)
 
@@ -69,7 +74,7 @@ export const Final = () => {
   }, [])
 
   return (
-    <div className="h-screen w-full flex items-center justify-center text-center flex-col gap-6">
+    <div className="h-screen w-full flex items-center justify-center text-center flex-col gap-6 select-none cursor-pointer">
       <Logo width="166" />
       <H1>Thank you</H1>
       <MutedP>
