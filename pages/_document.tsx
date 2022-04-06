3ec7c961f319cdc9166db2ad43bed52078071f48
Sara Vieira <sara.vieira@remote.com>
2022-04-06 11:30:10 +0200

    add lang to html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html>
-      <Head lang="en">
+    <Html lang="en">
+      <Head>
         <link rel="preconnect" href="https://rsms.me" crossOrigin="true" />
 
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
