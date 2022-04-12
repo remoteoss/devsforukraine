f95dd2dff755fe2b2773b4cc0212d8d1fb2b12dd
Sara Vieira <sara.vieira@remote.com>
2022-04-12 11:50:09 +0200

    add balance back

--- pages/api/payment/balance.ts
+++ pages/api/payment/balance.ts
@@ -6,10 +6,19 @@ import { prisma } from "../../../utils/prisma"
 
 
 const balance = async (req: NextApiRequest, res: NextApiResponse) => {
+    const balance = await prisma.donation.findMany({
+        include: {
+            User: true,
+        }
+    })
 
+    const all = balance.reduce((acc, cur) => acc + cur.amount, 0)
 
     res.send({
-        balance: 0
+        balance: all,
+        goal: GOAL,
+        percentage: parseFloat((all / GOAL * 100).toFixed(2)),
+        donations: balance.length
     })
 }
 
