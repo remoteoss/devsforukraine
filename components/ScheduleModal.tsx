183ae70f98c1d172ea3a3ddb5e70c5f4a0f1cb78
Sara Vieira <hey@iamsaravieira.com>
2022-04-24 13:01:55 +0200

    add fe

--- components/ScheduleModal.tsx
+++ components/ScheduleModal.tsx
@@ -62,7 +62,63 @@ const BESchedule = [
   },
 ]
 
+const FESchedule = [
+  {
+    time: time("4:00:00"),
+    speaker: "Charlie Gerard",
+    talk: "Simple JStures can go a long way",
+  },
+  {
+    time: time("4:25:00"),
+    speaker: "Addy Osmani",
+    talk: "The Future Of Performance Tooling",
+  },
+  {
+    time: time("5:00:00"),
+    speaker: "Ben Lesh",
+    talk: "Introduction to Reactive programming",
+  },
+  {
+    time: time("5:40:00"),
+    speaker: "Break",
+  },
+  {
+    time: time("6:00:00"),
+    speaker: "Ben Hong",
+    talk: "Third Time's the Charm: Taking Another Look at Vue 3",
+  },
+  {
+    time: time("6:30:00"),
+    speaker: "Natalia Tepluhina",
+    talk: "Local state and server cache: finding a balance",
+  },
+  {
+    time: time("7:05:00"),
+    speaker: "Break",
+  },
+  {
+    time: time("7:30:00"),
+    speaker: "Dan Abramov",
+    talk: "Q&A",
+  },
+  {
+    time: time("8:00:00"),
+    speaker: "Volodymyr Agafonkin",
+    talk: "Solving everyday problems with data visualization",
+  },
+  {
+    time: time("8:50:00"),
+    speaker: "Jessica Janiuk",
+    talk: "A RESTful API for Your Wellbeing",
+  },
+  {
+    time: time("9:20:00"),
+    speaker: "End",
+  },
+]
+
 export default function ScheduleModal({ params: { day } }: any) {
+  const schedule = day === 1 ? FESchedule : BESchedule
   return (
     <div>
       <div className="mt-3 text-center sm:mt-5">
@@ -74,32 +130,30 @@ export default function ScheduleModal({ params: { day } }: any) {
           <MotionLabel>All times are in your current timezone</MotionLabel>
         </Dialog.Title>
         <div className="flex justify-center">
-          {day === 1 ? null : (
-            <div className="">
-              {BESchedule.map((s) => (
-                <div
-                  className="grid 
+          <div>
+            {schedule.map((s) => (
+              <div
+                className="grid 
                   gap-x-2 grid-cols-[80px_1fr] text-left pb-3 my-3 border-b-[1px] border-devs-gray200"
-                  key={s.talk}
-                >
-                  <div className="font-semibold">
-                    {s.time.toLocaleTimeString("en-US", {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </div>
-                  <div>
-                    {s.speaker}
-
-                    <span className="text-devs-gray100">
-                      {" "}
-                      {s.talk && " - "} {s.talk}
-                    </span>
-                  </div>
+                key={s.talk}
+              >
+                <div className="font-semibold">
+                  {s.time.toLocaleTimeString("en-US", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
                 </div>
-              ))}
-            </div>
-          )}
+                <div>
+                  {s.speaker}
+
+                  <span className="text-devs-gray100">
+                    {" "}
+                    {s.talk && " - "} {s.talk}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
